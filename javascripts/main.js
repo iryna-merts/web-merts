733e03e215737956bd5e973199debd34a656dd0d
iryna-merts <irynamertsalo@gmaiil.com>
2019-11-07 02:00:24 +0200

    Navbar added
    Top stick navbar is added and linked to anchor on the same page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,22 @@
+/* nav */
+$(function() {
+    $(".nav li").on("click", function() {
+      $(".nav li").removeClass("active");
+      $(this).addClass("active");
+    });
+  });
+  $(document).ready(function() {
+    $(".img").mouseenter(function() {
+      $(this).addClass("hover");
+    })
+  
+    .mouseleave(function() {
+      $(this).removeClass("hover");
+    });
+  
+  });
+/* end of nav */
+
 /* slider */
 $('#carouselFade').carousel();
 /* end of slider */
